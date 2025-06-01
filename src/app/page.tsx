4526964f0629a770c67c6b4dc3f67ce6eceae86b
Juan Ramirez <juandiegoramirez071@gmail.com>
2025-06-01 18:58:26 -0500

    Enhance image handling and layout in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ export default function Home() {
   return (
 
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-lg">
+      <div className="w-lg border-[0.5px] rounded border-gray-800 p-5 h-[600px] max-h-[600px] overflow-y-auto flex flex-col space-y-2">
         <div className="chat chat-start">
           <div className="chat-image avatar">
-            <div className="w-15 rounded-full">
-              <img
-                alt="Tailwind CSS chat bubble component"
+            <div className="w-15">
+              <Image
+                className="rounded-full"
+                alt="AI"
+                fill
                 src="https://i.pinimg.com/222x/09/5c/44/095c44ce9421a10fad713a32d8648f6f.jpg"
               />
             </div>
@@ -21,9 +23,11 @@ export default function Home() {
         </div>
         <div className="chat chat-end">
           <div className="chat-image avatar">
-            <div className="w-15 rounded-full">
-              <img
-                alt="Tailwind CSS chat bubble component"
+            <div className="w-15">
+              <Image
+                className="rounded-full"
+                fill
+                alt="User"
                 src="https://i.pinimg.com/736x/92/20/88/922088e841d5d6c1bc9525bfdb7fe85e.jpg"
               />
             </div>
@@ -34,8 +38,16 @@ export default function Home() {
           <div className="chat-bubble">I hate you!</div>
         </div>
       </div>
-      <div>
-
+      <div className="w-lg">
+        <form className="flex w-full flex-col space-y-2">
+          <textarea
+            className="textarea textarea-bordered w-full focus:outline-none focus:border-none"
+            cols={30}
+            rows={10}
+            placeholder="type your message here..."
+          ></textarea>
+          <button className="btn btn-primary" type="submit" >Send</button>
+        </form>
       </div>
     </main>
   );
